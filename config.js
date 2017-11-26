9c71f7d077ba204c460820e9d722d73118f29465
Adam Rosenblum <adamR714@gmail.com>
2017-11-26 11:58:44 -0800

    changed config for Heoku

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL ||
-'mongodb://localhost:27017/crowdadventure';
+'mongodb://localhost/crowdadventure';
+// 'mongodb://localhost:27017/crowdadventure';
 
 exports.PORT = process.env.PORT || 8080;
